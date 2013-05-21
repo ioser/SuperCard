d69fe8d484a314d6120d5b2425a23403dd660970
Richard Millet <trioser@icloud.com>
2013-05-20 23:21:38 -0700

    Added code to draw the corners of the playing card.

--- SuperCard/PlayingCardView.m
+++ SuperCard/PlayingCardView.m
@@ -9,6 +9,8 @@
 #import "PlayingCardView.h"
 
 #define ROUNDED_CORNER_RADIANS 12.0
+#define CORNER_SCALE_FACTOR 0.20
+#define CORNER_OFFSET 2.0
 
 @implementation PlayingCardView
 
@@ -30,6 +32,52 @@
     return self;
 }
 
+- (void)pushContextAndRotate180 {
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextSaveGState(context);
+	
+	CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
+	CGContextRotateCTM(context, M_PI);
+}
+
+- (void)popContext {
+	CGContextRestoreGState(UIGraphicsGetCurrentContext());
+}
+
+- (NSString *)rankAsString {
+	return @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"][self.rank];
+}
+
+- (NSString *)getPlayingCardCornerString {
+	NSString *result = nil;
+	
+	result = [NSString stringWithFormat:@"%@\n%@", [self rankAsString], self.suit];
+	
+	return result;
+}
+
+- (void)drawPlayingCardCorners {
+	NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+	paragraphStyle.alignment = NSTextAlignmentCenter;
+	
+	UIFont *cornerFont = [UIFont systemFontOfSize:self.bounds.size.width * CORNER_SCALE_FACTOR];
+	NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:
+									  [self getPlayingCardCornerString] attributes:@{NSParagraphStyleAttributeName : paragraphStyle,
+														   NSFontAttributeName : cornerFont}];
+	
+	CGRect cornerTextBounds;
+	cornerTextBounds.origin.x = self.bounds.origin.x + CORNER_OFFSET;
+	cornerTextBounds.origin.y = self.bounds.origin.y + CORNER_OFFSET;
+	cornerTextBounds.size = [cornerText size];
+	
+	// Draw the upper left corner
+	[cornerText drawInRect:cornerTextBounds];
+	
+	// Draw the lower right corner
+	[self pushContextAndRotate180];
+	[cornerText drawInRect:cornerTextBounds];
+	[self popContext];
+}
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
@@ -44,6 +92,8 @@
 	
 	[[UIColor blackColor] setStroke];
 	[roundedRect stroke];
+	
+	[self drawPlayingCardCorners];
 }
 
 
